573d12fbf3381880c4363b07c89804e2b8d07cb4
PaulTreitel <paul.treitel@gmail.com>
2024-03-21 13:18:08 -0700

    bugfix

--- tpk/run_shop.py
+++ tpk/run_shop.py
@@ -11,17 +11,18 @@ REWARD_VALUES = [
 	[-4, 0, 2, 4],
 	[-10, 0, 5, 10]
 ]
+PROFIT_VALUES = [0, 10, 15, 25, 55, 110, 220, 450, 1600, 3200, 8000]
 
 
-def calculate_run_shop(int_lore, skill, diplomacy, pv):
-	dc10_expected = get_run_expected_value(DC10, int_lore, skill, diplomacy, pv)
-	dc15_expected = get_run_expected_value(DC15, int_lore, skill, diplomacy, pv)
-	dc20_expected = get_run_expected_value(DC20, int_lore, skill, diplomacy, pv)
-	dc25_expected = get_run_expected_value(DC25, int_lore, skill, diplomacy, pv)
+def calculate_run_shop(int_lore, skill, diplomacy, sl, haul):
+	dc10_expected = get_run_expected_value(DC10, int_lore, skill, diplomacy, sl, haul)
+	dc15_expected = get_run_expected_value(DC15, int_lore, skill, diplomacy, sl, haul)
+	dc20_expected = get_run_expected_value(DC20, int_lore, skill, diplomacy, sl, haul)
+	dc25_expected = get_run_expected_value(DC25, int_lore, skill, diplomacy, sl, haul)
 	print(f'DC10: {dc10_expected}\nDC15: {dc15_expected}\nDC20: {dc20_expected}\nDC25: {dc25_expected}')
 
 
-def get_run_expected_value(dc, int_lore, skill, diplomacy, pv, haul):
+def get_run_expected_value(dc, int_lore, skill, diplomacy, sl, haul):
 	total_value = 0
 	for i in range(1, 21):
 		for s in range(1, 21):
@@ -34,7 +35,7 @@ def get_run_expected_value(dc, int_lore, skill, diplomacy, pv, haul):
 				if d + diplomacy >= dc:
 					successes += 1
 				reward_level = (dc - 10) // 5
-				new_money = REWARD_VALUES[reward_level][successes] * pv
+				new_money = REWARD_VALUES[reward_level][successes] * PROFIT_VALUES[sl]
 				if haul and new_money < 0:
 					new_money = new_money // 2
 				total_value += new_money
@@ -45,7 +46,7 @@ def calculate_passive(sl, a, high_a, higher_a, haul, sister):
 	for i in range(1, 101):
 		new_money = get_passive_money(sl, i, a, high_a, higher_a)
 		if sister and new_money < 0:
-			new_money += calculate_passive(sl, i, a, high_a, higher_a, haul)
+			new_money += calculate_passive(sl, a, high_a, higher_a, False, False)
 		if haul and new_money < 0:
 			new_money = new_money // 2
 		total_value += new_money
@@ -85,19 +86,20 @@ def  get_passive_money(sl, i, a, high_a, higher_a):
 
 def get_expected_roll(dice, die_size, a, high_a, higher_a, double):
 	roll = 0
+	die_ev = (die_size / 2) + 1
 	for i in range(dice):
 		die_val = 0
-		for val in range(1, die_size + 1):
-			if higher_a and val == 1 or val == 2:
-				new_val = 0
-				for j in range(3, die_size + 1):
-					new_val += j
-				val = new_val / (die_size - 2)
-			elif (a and val == 1) or (high_a and val == 1 or val == 2):
-				new_val = 0
-				for j in range(1, die_size + 1):
-					new_val += j
-				val = new_val / die_size
+		if higher_a:
+			die_reroll_ev = sum([r for r in range(3, die_size + 1)]) / (die_size - 2)
+			die_val += 2 * die_reroll_ev
+		if high_a:
+			die_val += 2 * die_ev
+		if a:
+			die_val += die_ev + 2
+		else:
+			die_val += 3
+
+		for val in range(3, die_size + 1):
 			die_val += val
 		roll += die_val / die_size
 
@@ -112,16 +114,15 @@ def get_expected_roll(dice, die_size, a, high_a, higher_a, double):
 @click.option('-i', default=0, help='Int/Lore skill modifier')
 @click.option('-s', default=0, help='shop skill modifier')
 @click.option('-d', default=0, help='Diplomacy modifier')
-@click.option('-pv', default=10, help='Profit Value')
 @click.option('-sl', default=1, help='Shop Level')
 @click.option('-a', is_flag=True, help="flag for Assurance")
 @click.option('-high_a', is_flag=True, help="flag for High Quality Assurance")
 @click.option('-higher_a', is_flag=True, help="flag for Higher Qualityer Assurance")
 @click.option('-haul', is_flag=True, help="flag for Last Week's Haul")
 @click.option('-sister', is_flag=True, help="flag for Sister Location")
-def handler(mode, i, s, d, pv, sl, a, high_a, higher_a, haul, sister):
+def handler(mode, i, s, d, sl, a, high_a, higher_a, haul, sister):
 	if mode == 'run':
-		calculate_run_shop(i, s, d, pv, haul)
+		calculate_run_shop(i, s, d, sl, haul)
 	elif mode == 'passive':
 		expected_value = calculate_passive(sl, a, high_a, higher_a, haul, sister)
 		print('Expected value: {:.3f}'.format(expected_value))
